ffd8af8f73b566879aff3148b736f30b5cf50352
Dung Pham <dungpham197qn@gmail.com>
2025-05-12 08:03:56 +0700

    1

---
+++ SS5/src/main/java/model/Bai1/Product.java
@@ -0,0 +1,14 @@
+package model.Bai1;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+}
